d3d197cbe527c143115d98fdd2a5f41697162b17
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-08-01 12:14:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,11 +20,22 @@ function modelLoaded() {
 }
 
 function draw() {
-    background('#969A97');
+    background('#F8F8FF');
+    document.getElementById("square_side").innerHTML = "The width and height of the square will be " + difference + "px!";
+    fill('#BFEFFF');
+    stroke('#969A97');
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + ", Nose Y = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("LeftWrist X = " + leftWristX + ", RightWrist X = " + rightWristX + ", Difference = " + difference);
     }
 }
